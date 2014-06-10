d7452ace495f11630b65976b01a89846053281a1
Eric Griffin <eg@fluenttech.net>
2014-06-10 12:26:23 -0400

    Optimized imports. Improved comments.

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import os, sys
+# netconfigit
+# network device configuration archiver
+
+import sys
 import os.path
-import time
-import threading
 import signal
-from xml.dom import minidom
+import logging
 import getopt
-from modules import tftpy
+
 from git import *
 import paramiko
-from modules import threadpool
-import logging
+
 from modules import netconfigit
 from modules import aescrypt
 
 
+# define a global logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 def signal_handler(signal, frame):
-    """
-
+    """Handles OS signals
 
     @rtype : object
     @param signal:
@@ -32,37 +32,41 @@ def signal_handler(signal, frame):
 
 
 def usage(error):
-    """
+    """Displays command-line argument usage and a specified error message
 
-    :param error:
+    :param error: the error message stating what went wrong
     """
     print(error)
     print("\nUsage: %s -c [configuration file] -p [password] (-e [plaintext]) | -d [ciphertext]) (-v)\n" % sys.argv[0])
     exit(0)
 
 
+# main entry point
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
+    # global variables
     configuration = ""
     password = ""
     verbose = 0
     opts = ""
 
+    # define command-line flags
     try:
         opts, args = getopt.getopt(sys.argv[1:], "c:p:ve:d:")
     except getopt.GetoptError as e:
         print (str(e))
-        usage()
+        usage("")
 
+    # determine which flags were called and assign parameters
     for o, a in opts:
-        if o == '-c':
+        if o == '-c':       # configuration file
             configuration = a
-        elif o == '-p':
+        elif o == '-p':     # decryption password
             password = a
-        elif o == '-v':
+        elif o == '-v':     # verbose output
             verbose = 1
-        elif o == '-e':
+        elif o == '-e':     # encrypt plaintext password given on command-line
             if password == "":
                 usage("Encryption password needs to be specified with -p [password]\n")
             input = a
@@ -70,7 +74,7 @@ if __name__ == "__main__":
             encoded = password.encode(input)
             print "\nEncrypted form of \"" + input + "\" is:\n" + encoded
             exit(0)
-        elif o == '-d':
+        elif o == '-d':     # decrypt ciphertext password given on command-line
             if password == "":
                 usage("Encryption password needs to be specified with -p [password]\n")
             input = a
@@ -80,18 +84,17 @@ if __name__ == "__main__":
         else:
             assert False, "unhandled option"
 
-    # check command-line syntax
+    # ensure that a password is specified
     if password == "":
         usage("\nNo password specified")
 
-    # make sure configuration file exists
+    # ensure that the specified configuration file exists
     if not os.path.isfile(configuration):
         usage("\nConfiguration file %s does not exist - quitting.\n" % configuration)
 
+    # create the neconfigit object
+    # the specified configuration file is parsed at this point
     nc = netconfigit.netconfigit(configuration, password)
-
-    if verbose == 1:
-        nc.verbose = 1
 
     # set logging options
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -100,17 +103,23 @@ if __name__ == "__main__":
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    if nc.verbose == 1:
+    # set verbosity
+    if verbose == 1:
+        nc.verbose = 1
         paramiko.util.log_to_file(os.path.splitext(nc.logfile)[0] + '_transport.log')
 
+    # run the netconfigit object
+    # this initiates the running of the actions specified for each device
     nc.run_nc()
 
-    #while nc.device_threadpool.tasks.unfinished_tasks > 0:
-    #    print "working..."
+    # here you have the option to do something while the device action threads are running
+    while nc.device_threadpool.tasks.unfinished_tasks > 0:
+        working = 1
 
+    # initiates the clean-up of the netconfigit run
+    # waits for all threads to finish and moves config files
     nc.stop_nc()
 
+    # if repository directory is a git repo, perform a commit/pull/push to synchronize everything
     if nc.using_git == 1:
         nc.git_commit_push(nc.repository)
-
-
